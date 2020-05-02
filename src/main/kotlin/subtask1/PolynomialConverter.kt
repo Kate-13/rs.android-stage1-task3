cd0b622ba11e3f54d4e736ea9e98d05ec69a7b44
Kate-13 <kate_boyko@mail.ru>
2020-05-02 19:54:06 +0300

    feat: add function to implement PolynomialConverter

--- src/main/kotlin/subtask1/PolynomialConverter.kt
+++ src/main/kotlin/subtask1/PolynomialConverter.kt
@@ -1,9 +1,46 @@
 package subtask1
 
+import kotlin.math.absoluteValue
+
 class PolynomialConverter {
 
     // TODO: Complete the following function
     fun convertToStringFrom(numbers: Array<Int>): String? {
-        throw NotImplementedError("Not implemented")
+        var r = ""
+        var res: String? = ""
+        if (numbers.isNullOrEmpty()) res = null
+        else {
+            numbers.forEachIndexed { i, num ->
+                val a = num.absoluteValue
+                val st = numbers.size - i - 1
+                if (i == 0) {
+                    if (st == 0) r = "$a"
+                    else r = "$a" + "x^" + "$st"
+                }
+                if (i > 0) {
+                    if (num == 0) r = ""
+                    if (num == -1) {
+                        if (st == 1) r = " - x"
+                        else r = " - x^" + "$st"
+                    }
+                    if (num == 1) {
+                        if (st == 1) r = " + x"
+                        else r = " + x^" + "$st"
+                    }
+                    if (num > 0 && num != 1) {
+                        if (st == 1) r = " + $a" + "x"
+                        else if (st == 0) r = " + $a"
+                            else r = " + $a" + "x^" + "$st"
+                    }
+                    if (num < 0 && num != -1) {
+                        if (st == 1) r = " - $a" + "x"
+                        else if (st == 0) r = " - $a"
+                            else r = " - $a" + "x^" + "$st"
+                    }
+                }
+                res += r
+            }
+        }
+        return res
     }
 }
