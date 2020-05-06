d5ab637c51ea5dca1b11c6687747c2ba6f460881
Kate-13 <kate_boyko@mail.ru>
2020-05-06 22:14:12 +0300

    feat: add function to implement SquareDecomposer

--- src/main/kotlin/subtask4/SquareDecomposer.kt
+++ src/main/kotlin/subtask4/SquareDecomposer.kt
@@ -1,9 +1,38 @@
 package subtask4
 
+import kotlin.math.roundToInt
+
 class SquareDecomposer {
 
-    // TODO: Complete the following function
+
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+
+            val numsqr = Math.pow(number.toDouble(), 2.0)
+            val resarr: ArrayList<Int>? = arrayListOf()
+            val arr = arrayListOf<Double>()
+            var nextel = number - 1
+            resarr?.add(nextel)
+            var restsum = numsqr - Math.pow(nextel.toDouble(), 2.0)
+            if (number > 0) {
+
+                while (restsum > 0) {
+                    var sqrtel = Math.sqrt(restsum)
+                    var next = Math.floor(sqrtel)
+                    resarr?.add(next.toInt())
+                    resarr?.sort()
+                    println(resarr)
+                    restsum = restsum - Math.pow(next, 2.0)
+
+                }
+            } else return null
+            resarr?.forEachIndexed { i , el ->
+            if (resarr?.size > 1) {
+                if (el < resarr[i+1]){
+                    println(resarr)
+                    return resarr?.toTypedArray()
+                }else return null
+            }
+        }
+        return resarr?.toTypedArray()
     }
 }
